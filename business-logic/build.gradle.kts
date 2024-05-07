01552aa5e8852a9ca73c4c17cb3e76a8c46ae150
maxwen <max.weninger@gmail.com>
2024-05-07 17:12:54 +0200

    testing

--- business-logic/build.gradle.kts
+++ business-logic/build.gradle.kts
@@ -6,11 +6,16 @@ dependencies {
     implementation(platform("com.example:platform"))
     implementation(project(":data-model"))
 
-//    implementation("org.slf4j:slf4j-api")
-    implementation(libs.slf4j.api)
+    implementation("org.slf4j:slf4j-api")
+//    implementation(libs.slf4j.api)
 
-//    implementation("org.apache.commons:commons-lang3")
-    implementation(libs.commons.lang)
+    implementation("org.apache.commons:commons-lang3")
+//    implementation(libs.commons.lang)
 
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    integrationTestImplementation("org.junit.jupiter:junit-jupiter-api")
+    integrationTestRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -2,10 +2,32 @@ import com.example.gradle.Slf4JSimpleRule
 import gradle.kotlin.dsl.accessors._a36adeff7943c18d5e9e07d718caef3f.compileJava
 import gradle.kotlin.dsl.accessors._a36adeff7943c18d5e9e07d718caef3f.compileTestJava
 import gradle.kotlin.dsl.accessors._a36adeff7943c18d5e9e07d718caef3f.javadoc
+import gradle.kotlin.dsl.accessors._a36adeff7943c18d5e9e07d718caef3f.sourceSets
+import gradle.kotlin.dsl.accessors._f8ba0958e671444bc04a21941c7f7ed6.build
+import org.gradle.kotlin.dsl.provider.inLenientMode
 
 plugins {
     id("java")
     id("com.diffplug.spotless")
+}
+
+//sourceSets.main {
+////    java.setSrcDirs(listOf(layout.projectDirectory.dir("sources")))
+//}
+//sourceSets.test {
+//
+//}
+
+sourceSets.create("integrationTest")
+
+tasks.register<Test>("integrationTest") {
+    group = "My Group"
+    description = "integration test"
+
+    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+    classpath = sourceSets["integrationTest"].runtimeClasspath
+
+    useJUnitPlatform()
 }
 
 java {
@@ -22,7 +44,27 @@ tasks.compileTestJava {
 }
 
 tasks.test {
+    useJUnitPlatform {
+        excludeTags("slow")
+    }
+    maxParallelForks = 4
+    maxHeapSize = "1g"
+}
 
+tasks.register<Test>("testSlow") {
+    group = "My Group"
+    description = "test slow"
+
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+
+    useJUnitPlatform {
+        includeTags("slow")
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.named("testSlow"))
 }
 
 tasks.javadoc {
